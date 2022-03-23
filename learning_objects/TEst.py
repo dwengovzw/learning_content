24a925522d66844fab18e767a8e16bbce5552dbb
BjarneVdv <85639481+BjarneVdv@users.noreply.github.com>
2022-03-23 23:19:02 +0100

    Temp

--- learning_objects/TEst.py
+++ learning_objects/TEst.py
@@ -1,17 +1,17 @@
 #include <Wire.h>
 #include <Dwenguino.h>
 #include <LiquidCrystal.h>
-#define SOUND_SENSOR_PIN_A3 A3
+#define SOUND_SENSOR_PIN_12 12
 void setup()
 {
   initDwenguino();
-  pinMode(SOUND_SENSOR_PIN_A3, INPUT);
+  pinMode(SOUND_SENSOR_PIN_12, INPUT);
 }
 
 void loop()
 {
     dwenguinoLCD.clear();
     dwenguinoLCD.setCursor(0,0);
-    dwenguinoLCD.print((digitalRead(SOUND_SENSOR_PIN_A3)) + String("db"));
+    dwenguinoLCD.print((digitalRead(SOUND_SENSOR_PIN_12)) + String("db"));
     delay(100);
 }
